e524a913a194f3e8aed9427c943602b7346ebf4a
Himanshu Pandey <himanshup895@gmail.com>
2018-11-17 19:43:09 +0530

    reducer added for add and delete items

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -13,11 +13,16 @@ const initialState = {
 }
 
 export default function(state = initialState, action) {
-    switch(action,type) {
+    switch(action.type) {
         case GET_ITEMS:
             return {
                 ...state
-            }
+            };
+        case DELETE_ITEMS:
+            return {
+                ...state,
+                items: state.items.filter(item => item.id !== action.payload)
+            };
         default: 
             return state;
     }
