5a653284b8cc95cea1bd885a041b382c55610743
Himanshu Pandey <himanshup895@gmail.com>
2018-11-17 17:52:41 +0530

    Redux implementation

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,24 @@
+import uuid from 'uuid';
+import {GET_ITEMS, ADD_ITEMS, DELETE_ITEMS} from '../actions/types';
+import { type } from 'os';
+
+const initialState = {
+    items: [
+        {id: uuid(), name: 'Eggs'},
+        {id: uuid(), name: 'Milks'},
+        {id: uuid(), name: 'Chocolate'},
+        {id: uuid(), name: 'Biscuits'},
+        {id: uuid(), name: 'Namkeen'}
+    ] 
+}
+
+export default function(state = initialState, action) {
+    switch(action,type) {
+        case GET_ITEMS:
+            return {
+                ...state
+            }
+        default: 
+            return state;
+    }
+}
